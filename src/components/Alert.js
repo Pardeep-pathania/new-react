b5324cd24be97ea71dccf0022adb090b9d056887
pardeep-pathania <pathanianikhil145@gmail.com>
2025-01-16 18:07:47 +0530

    remove almost every problem

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Alert(props) {
-    const capitalize = (word) =>{
+    const capitalise = (word) =>{
         const lower = word.toLowerCase();
         return lower.charAt(0).toUpperCase() + lower.slice(1);
     }
@@ -9,8 +9,8 @@ function Alert(props) {
   return (
     <div style= {{height: '50px'}}>
       {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-        <strong>{capitalize(props.alert.type)}</strong>:{props.alert.msg}
-      </div>}
+        <strong>{capitalise(props.alert.type)}</strong>:{props.alert.msg}
+      </div>} 
       </div>
         )
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,11 +2,11 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
 
-    function handleUpClick() {
+    const handleUpClick=()=>{
        // console.log("Uppercase was clicked" + text);
         let newText = text.toUpperCase();
         setText(newText);
-        props.showAlert("Converted to uppercase");
+        props.showAlert("Converted to uppercase","success");
     }  
 
     const handleLoClick =()=>{
@@ -28,9 +28,8 @@ export default function TextForm(props) {
 
     const handleCopy = () => {
       //console.log("Text Copy")
-      var text = document.getElementById("myBox");
-      text.select();
-      navigator.clipboard.writeText(text.value);
+      navigator.clipboard.writeText(text);
+      document.getSelection().removeAllRanges();
       props.showAlert("Text copied to clipboard","success")
     }
 
@@ -58,10 +57,10 @@ export default function TextForm(props) {
 
     <div className="container my-3"style={{color:props.mode=== 'dark'?'white':'black'}} >
         <h2>Your Text summary</h2>
-        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+        <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
         <h3>Preview</h3>
-        <p>{text.length>0?text:"Enter something to preview"}</p>
+        <p>{text.length>0?text:"Nothing to preview"}</p>
 
     </div>
     </>
